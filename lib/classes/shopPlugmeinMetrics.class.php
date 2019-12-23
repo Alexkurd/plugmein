402996df32d06e0e691bf6d08a86838ffed66d37
Alexkurd <7689609+Alexkurd@users.noreply.github.com>
2019-12-23 23:35:02 +0300

    Metrics

---
+++ lib/classes/shopPlugmeinMetrics.class.php
@@ -0,0 +1,162 @@
+<?php
+
+/**
+ * Class shopPlugmeinMetrics
+ * Send anonymous user stats
+ */
+class shopPlugmeinMetrics
+{
+    private $apps = ['shop', 'mylang'];
+    private $data = [];
+    private $id; //uuid
+    private $mysql_version;
+    const DEMOLLC_STAT_SERVER = 'https://demollc.pw/stat/';
+
+    public function __construct()
+    {
+        $model = new waAppSettingsModel();
+        $id = $model->get('shop.plugmein', 'uuid');
+        if (!$id) {
+            $id = waString::uuid();
+            $model->set('shop.plugmein', 'uuid', $id);
+        }
+        $this->id = $id;
+    }
+
+    public function sendBeacon()
+    {
+        $data = $this->getData();
+        $options = [
+            'format' => waNet::FORMAT_JSON,
+            'request_format' => waNet::FORMAT_JSON,
+            'timeout' => '5',
+        ];
+        $net = new waNet($options);
+        $net->query(self::DEMOLLC_STAT_SERVER, $data, waNet::METHOD_PUT);
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return array
+     * @throws waException
+     */
+    public function getData()
+    {
+        $this->data['system'] = $this->getSystemData();
+        foreach ($this->apps as $app) {
+            $method = 'get'.ucfirst($app).'Data';
+            if (method_exists($this, $method) && wa()->appExists($app)) {
+                $this->data['apps'][$app] = $this->$method();
+            }
+        }
+        return $this->data;
+    }
+
+
+    /**
+     * @return array
+     * @throws waException
+     */
+    private function getSystemData()
+    {
+        return [
+            'php_version' => str_replace(PHP_EXTRA_VERSION, '', PHP_VERSION),
+            'mysql_version' => $this->getMysqlVersion(),
+            'utfmb4' => $this->mb4IsSupported(),
+            'id' => $this->id,
+            'timestamp' => time(),
+            'locales' => waLocale::getAll(),
+            'is_cloud' => wa()->appExists('hosting'),
+        ];
+    }
+
+    /**
+     * @return array
+     * @throws waDbException
+     * @throws waException
+     */
+    private function getMylangData()
+    {
+        wa('mylang');
+        $mm = new mylangModel();
+        return [
+            'id' => 'mylang',
+            'app' => 'mylang',
+            'version' => wa()->getVersion('mylang'),
+            'counters' => $mm->getCount(),
+        ];
+    }
+
+    /**
+     * @return array
+     * @throws waDbException
+     * @throws waException
+     */
+    private function getShopData()
+    {
+        $pm = new shopProductModel();
+        $fm = new shopFeatureModel();
+        $this->getShopPlugins();
+        return [
+            'id' => 'shop',
+            'app' => 'shop',
+            'version' => wa()->getVersion('shop'),
+            'products' => $pm->countAll(),
+            'features' => $fm->countAll(),
+        ];
+    }
+
+    private function getShopPlugins()
+    {
+        $plugins = wa('shop')->getConfig()->getPlugins();
+        foreach ($plugins as $id=>$p) {
+            if ($p['vendor'] === '991739') {
+                $this->data['plugins'][$id] = [
+                    'id' => $id,
+                    'app' => 'shop',
+                    'version' => ifset($p, 'version', 0)
+                ];
+            }
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function previewData()
+    {
+        $this->getData();
+        print_r($this->data);
+    }
+
+    /**
+     * @return mixed|null
+     * @throws waDbException
+     * @throws waException
+     */
+    protected function getMysqlVersion()
+    {
+        if (!$this->mysql_version) {
+            $model = new waModel();
+            $version = $model->query('SELECT VERSION()')->fetchRow();
+            $this->mysql_version = !empty($version[0]) ? $version[0] : null;
+
+        }
+        return $this->mysql_version;
+    }
+
+    /**
+     * @return bool
+     */
+    protected function mb4IsSupported()
+    {
+        return version_compare($this->mysql_version, '5.5.3', '>=');
+    }
+}
